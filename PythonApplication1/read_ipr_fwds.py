5908672ed3155419eda826330d50346f16baccd4
Jonathan Slack <jslack31@gatech.edu>
2017-10-26 23:36:10 -0500

    Finished printing code for read_ipr_fwds

--- PythonApplication1/read_ipr_fwds.py
+++ PythonApplication1/read_ipr_fwds.py
@@ -124,124 +124,6 @@ def pull_iprdata_ff(file_out, dir, fold):
         if no_issues != "": 
             ipr_data[key]["no_issues"] = bin_trans_rev[no_issues]
 
-def write_ipr_data2(data_in, keys, keys2, file_out):
-    # Delete existing file if it exists
-    if os.path.isfile(file_out) == True:
-        os.remove(file_out)     
-
-    # Open a workbook and our first worksheet for prior data
-    workbook = xlsxwriter.Workbook(file_out)
-    worksheet = workbook.add_worksheet(name = "IPR Trial Data")
-    worksheet.set_column(0,0,20)
-    worksheet.set_column(2,2,15)
-    worksheet.set_column(5,6,12)
-    worksheet.set_column(8,9,40)
-    worksheet.set_column(4,4,20)
-    worksheet.set_column(3,3,8)
-    worksheet.set_column(7,7,8)
-    worksheet.set_column(11,11,20)
-
-    # Set formats
-    header_format = workbook.add_format({'bold': True, 'font_color': 'black', 'align' : 'center'})
-    header_format.set_text_wrap()
-    text_format = workbook.add_format({'font_color': 'black', 'align' : 'vcenter'})
-    text_format.set_text_wrap()
-    text_format.set_align('center')
-    text_format_sm = workbook.add_format({'font_color': 'black', 'align' : 'vcenter'})
-    text_format_sm.set_text_wrap()
-    text_format_sm.set_align('center')
-    text_format_sm.set_font_size(10)
-    date1_format = workbook.add_format({'font_color': 'black', 'num_format':'yyyy/mm/dd', 'align' : 'vcenter'})
-    date1_format.set_text_wrap()
-    date1_format.set_align('center')
-    date2_format = workbook.add_format({'font_color': 'black', 'num_format':'mm/dd/yyyy', 'align' : 'vcenter'})
-    date2_format.set_text_wrap()
-    date2_format.set_align('center')
-    int_format = workbook.add_format({'font_color': 'black', 'num_format':'0', 'align' : 'vcenter'})
-    int_format.set_text_wrap() 
-    int_format.set_align('center')
-    float_format = workbook.add_format({'font_color': 'black', 'num_format':'0.00', 'align' : 'vcenter'})
-    float_format.set_text_wrap()
-    float_format.set_align('center')
-    special_format = workbook.add_format({'font_color': 'red', 'align' : 'vcenter'})
-    special_format.set_text_wrap()
-    special_format.set_align('center')
-    
-    # Write headers for our first worksheet:
-    worksheet.write('A1', 'Trial Number(s)', header_format)
-    worksheet.write('B1', 'Trial Type', header_format)
-    worksheet.write('C1', 'Trial Date', header_format)
-    worksheet.write('D1', 'Final Written Decision?', header_format)
-    worksheet.write('E1', 'Decision Type(s)', header_format)
-    worksheet.write('F1', 'Associated Patent(s)', header_format)
-    worksheet.write('G1', 'Associated Patent Type(s)', header_format)
-    worksheet.write('H1', 'Multiple Patents?', header_format)
-    worksheet.write('I1', 'Petitioner Name(s)', header_format)
-    worksheet.write('J1', 'Patent Holder Name(s)', header_format)
-    worksheet.write('K1', 'Issues?', header_format)
-    worksheet.write('L1', 'Filename', header_format)
-
-    # Write in data for each ipr on our first worksheet:
-    bin_trans = {True: "Yes", False: "No", None: "No"}
-    bin_trans_rev = {True: "No", False: "Yes", None: "Yes"}
-    row = 1
-    for key in keys:
-        if data_in[key]["trial_num(s)"] is not None:
-            worksheet.write_string(row,0,"\n".join(data_in[key]["trial_num(s)"]),text_format)
-        if data_in[key]["trial_type"] is not None:
-            worksheet.write_string(row,1,data_in[key]["trial_type"],text_format)
-        if data_in[key]["dec_date"] is not None:
-            worksheet.write_string(row,2,data_in[key]["dec_date"],text_format)
-        worksheet.write_string(row,3,bin_trans[data_in[key]["FWD?"]],text_format)
-        if data_in[key]["fd_type(s)"] is not None:
-            worksheet.write_string(row,4,data_in[key]["fd_type(s)"],text_format)
-        if data_in[key]["pat_num(s)"] is not None:
-            worksheet.write_string(row,5,"\n".join(data_in[key]["pat_num(s)"]),text_format)
-        if data_in[key]["pat_type(s)"] is not None:
-            worksheet.write_string(row,6,"\n".join(data_in[key]["pat_type(s)"]),text_format)
-        worksheet.write_string(row,7,bin_trans[data_in[key]["mult_pat"]],text_format)
-        if data_in[key]["pet_name(s)"] is not None:
-            worksheet.write_string(row,8,"\n".join(data_in[key]["pet_name(s)"]),text_format_sm)
-        if data_in[key]["ph_name(s)"] is not None:
-            worksheet.write_string(row,9,"\n".join(data_in[key]["ph_name(s)"]),text_format_sm)
-        worksheet.write_string(row,10,bin_trans_rev[data_in[key]["no_issues"]],text_format)
-        worksheet.write_string(row,11,key,text_format_sm)
-        row += 1
-
-    # Open a workbook and our first worksheet for prior data
-    worksheet2 = workbook.add_worksheet(name = "IPR Patent Data")
-    worksheet2.set_column(0,0,20)
-    worksheet2.set_column(1,1,8)
-    worksheet2.set_column(2,2,15)
-    worksheet2.set_column(3,3,20)
-    worksheet2.set_column(4,4,40)
-    worksheet2.set_column(5,5,60)
-    worksheet2.set_column(6,7,12)
-    worksheet2.set_column(8,8,5)
-    worksheet2.set_column(9,9,15)
-    worksheet2.set_column(10,10,15)
-
-    # Write headers for our second worksheet:
-    worksheet2.write('A1', 'Trial Number(s)', header_format)
-    worksheet2.write('B1', 'Trial Type', header_format)
-    worksheet2.write('C1', 'Trial Date', header_format)
-    worksheet2.write('D1', 'Decision Type(s)', header_format)
-    worksheet2.write('E1', 'Trial Patent Holder Name(s)', header_format)  
-    worksheet2.write('F1', 'Order Text', header_format)  
-    worksheet2.write('G1', 'Associated Patent', header_format)
-    worksheet2.write('H1', 'Associated Patent Type', header_format) 
-    worksheet2.write('I1', 'Affected Claim(s)', header_format)
-    worksheet2.write('J1', 'Claim(s) Disposition', header_format)
-    worksheet2.write('K1', 'Order Issues?', header_format)
-    worksheet2.write('L1', 'Claim Disp. Issues?', header_format)
-    worksheet2.write('M1', 'Multi-Page Order?', header_format)
-    worksheet2.write('N1', 'Filename', header_format)
-
-    # Write in data for each ipr on our second worksheet:
-    row = 1
-    for key2 in keys2:
-        pass
-
 def pat_type_check(list_of_nums):
     # Helper function for screening out list of patents with PAT and not RE, D, etc.
     first3 = np.asarray([el[0:3] for el in list_of_nums[:]])
@@ -750,7 +632,152 @@ def order_claim_disposition(order_set, target):
                 print(string_of_nums)
                 print(patnum)
 
-    return dispos, False, error_flag     
+    return dispos, False, error_flag    
+
+def write_ipr_data2(data_in, keys, keys2, file_out):
+    # Delete existing file if it exists
+    if os.path.isfile(file_out) == True:
+        os.remove(file_out)     
+
+    # Open a workbook and our first worksheet for prior data
+    workbook = xlsxwriter.Workbook(file_out)
+    worksheet = workbook.add_worksheet(name = "IPR Trial Data")
+    worksheet.set_column(0,0,20)
+    worksheet.set_column(2,2,15)
+    worksheet.set_column(5,6,12)
+    worksheet.set_column(8,9,40)
+    worksheet.set_column(4,4,20)
+    worksheet.set_column(3,3,8)
+    worksheet.set_column(7,7,8)
+    worksheet.set_column(11,11,20)
+
+    # Set formats
+    header_format = workbook.add_format({'bold': True, 'font_color': 'black', 'align' : 'center'})
+    header_format.set_text_wrap()
+    text_format = workbook.add_format({'font_color': 'black', 'align' : 'vcenter'})
+    text_format.set_text_wrap()
+    text_format.set_align('center')
+    text_format_sm = workbook.add_format({'font_color': 'black', 'align' : 'vcenter'})
+    text_format_sm.set_text_wrap()
+    text_format_sm.set_align('center')
+    text_format_sm.set_font_size(10)
+    date1_format = workbook.add_format({'font_color': 'black', 'num_format':'yyyy/mm/dd', 'align' : 'vcenter'})
+    date1_format.set_text_wrap()
+    date1_format.set_align('center')
+    date2_format = workbook.add_format({'font_color': 'black', 'num_format':'mm/dd/yyyy', 'align' : 'vcenter'})
+    date2_format.set_text_wrap()
+    date2_format.set_align('center')
+    int_format = workbook.add_format({'font_color': 'black', 'num_format':'0', 'align' : 'vcenter'})
+    int_format.set_text_wrap() 
+    int_format.set_align('center')
+    float_format = workbook.add_format({'font_color': 'black', 'num_format':'0.00', 'align' : 'vcenter'})
+    float_format.set_text_wrap()
+    float_format.set_align('center')
+    special_format = workbook.add_format({'font_color': 'red', 'align' : 'vcenter'})
+    special_format.set_text_wrap()
+    special_format.set_align('center')
+    
+    # Write headers for our first worksheet:
+    worksheet.write('A1', 'Trial Number(s)', header_format)
+    worksheet.write('B1', 'Trial Type', header_format)
+    worksheet.write('C1', 'Trial Date', header_format)
+    worksheet.write('D1', 'Final Written Decision?', header_format)
+    worksheet.write('E1', 'Decision Type(s)', header_format)
+    worksheet.write('F1', 'Associated Patent(s)', header_format)
+    worksheet.write('G1', 'Associated Patent Type(s)', header_format)
+    worksheet.write('H1', 'Multiple Patents?', header_format)
+    worksheet.write('I1', 'Petitioner Name(s)', header_format)
+    worksheet.write('J1', 'Patent Holder Name(s)', header_format)
+    worksheet.write('K1', 'Issues?', header_format)
+    worksheet.write('L1', 'Filename', header_format)
+
+    # Write in data for each ipr on our first worksheet:
+    bin_trans = {True: "Yes", False: "No", None: "No"}
+    bin_trans_rev = {True: "No", False: "Yes", None: "Yes"}
+    row = 1
+    for key in keys:
+        if data_in[key]["trial_num(s)"] is not None:
+            worksheet.write_string(row,0,"\n".join(data_in[key]["trial_num(s)"]),text_format)
+        if data_in[key]["trial_type"] is not None:
+            worksheet.write_string(row,1,data_in[key]["trial_type"],text_format)
+        if data_in[key]["dec_date"] is not None:
+            worksheet.write_string(row,2,data_in[key]["dec_date"],text_format)
+        worksheet.write_string(row,3,bin_trans[data_in[key]["FWD?"]],text_format)
+        if data_in[key]["fd_type(s)"] is not None:
+            worksheet.write_string(row,4,data_in[key]["fd_type(s)"],text_format)
+        if data_in[key]["pat_num(s)"] is not None:
+            worksheet.write_string(row,5,"\n".join(data_in[key]["pat_num(s)"]),text_format)
+        if data_in[key]["pat_type(s)"] is not None:
+            worksheet.write_string(row,6,"\n".join(data_in[key]["pat_type(s)"]),text_format)
+        worksheet.write_string(row,7,bin_trans[data_in[key]["mult_pat"]],text_format)
+        if data_in[key]["pet_name(s)"] is not None:
+            worksheet.write_string(row,8,"\n".join(data_in[key]["pet_name(s)"]),text_format_sm)
+        if data_in[key]["ph_name(s)"] is not None:
+            worksheet.write_string(row,9,"\n".join(data_in[key]["ph_name(s)"]),text_format_sm)
+        worksheet.write_string(row,10,bin_trans_rev[data_in[key]["no_issues"]],text_format)
+        worksheet.write_string(row,11,key,text_format_sm)
+        row += 1
+
+    # Open a workbook and our first worksheet for prior data
+    worksheet2 = workbook.add_worksheet(name = "IPR Patent Data")
+    worksheet2.set_column(0,0,20)
+    worksheet2.set_column(1,1,8)
+    worksheet2.set_column(2,2,15)
+    worksheet2.set_column(3,3,20)
+    worksheet2.set_column(4,4,40)
+    worksheet2.set_column(5,5,60)
+    worksheet2.set_column(6,7,12)
+    worksheet2.set_column(8,8,15)
+    worksheet2.set_column(9,9,10)
+    worksheet2.set_column(13,13,15)
+
+    # Write headers for our second worksheet:
+    worksheet2.write('A1', 'Trial Number(s)', header_format)
+    worksheet2.write('B1', 'Trial Type', header_format)
+    worksheet2.write('C1', 'Trial Date', header_format)
+    worksheet2.write('D1', 'Decision Type(s)', header_format)
+    worksheet2.write('E1', 'Trial Patent Holder Name(s)', header_format)  
+    worksheet2.write('F1', 'Order Text', header_format)  
+    worksheet2.write('G1', 'Associated Patent', header_format)
+    worksheet2.write('H1', 'Associated Patent Type', header_format) 
+    worksheet2.write('I1', 'Affected Claim(s)', header_format)
+    worksheet2.write('J1', 'Claim(s) Disposition', header_format)
+    worksheet2.write('K1', 'Order Issues?', header_format)
+    worksheet2.write('L1', 'Claim Disp. Issues?', header_format)
+    worksheet2.write('M1', 'Multi-Page Order?', header_format)
+    worksheet2.write('N1', 'Filename', header_format)
+
+    # Write in data for each ipr on our second worksheet:
+    row = 1
+    for key in keys2:
+        if data_in[key]["trial_num(s)"] is not None:
+            worksheet2.write_string(row,0,"\n".join(data_in[key]["trial_num(s)"]),text_format)
+        if data_in[key]["trial_type"] is not None:
+            worksheet2.write_string(row,1,data_in[key]["trial_type"],text_format)
+        if data_in[key]["dec_date"] is not None:
+            worksheet2.write_string(row,2,data_in[key]["dec_date"],text_format)
+        if data_in[key]["fd_type(s)"] is not None:
+            worksheet2.write_string(row,3,data_in[key]["fd_type(s)"],text_format)
+        if data_in[key]["ph_name(s)"] is not None:
+            worksheet2.write_string(row,4,"\n".join(data_in[key]["ph_name(s)"]),text_format_sm)
+        if data_in[key]["order_txt"] is not None:
+            worksheet2.write_string(row,5,data_in[key]["order_txt"],text_format)
+        row_old = row
+        i = 0
+        for pat_number in data_in[key]["pat_num(s)"]:
+            row += 1
+            worksheet2.write_string(row,6,pat_number,text_format)
+            worksheet2.write_string(row,7,data_in[key]["pat_type(s)"][i],text_format)
+            if data_in[key]["order_disp(s)"][pat_number]["c-range"] is not None:
+                worksheet2.write_string(row,8,"\n".join(data_in[key]["order_disp(s)"][pat_number]["c-range"]),text_format)
+            if data_in[key]["order_disp(s)"][pat_number]["disposition"] is not None:
+                worksheet2.write_string(row,9,"\n".join(data_in[key]["order_disp(s)"][pat_number]["disposition"]),text_format)
+            worksheet2.write_string(row,10,bin_trans_rev[data_in[key]["no_issues2"]],text_format)
+            worksheet2.write_string(row,11,bin_trans[data_in[key]["expect_ccd"]],text_format)
+            worksheet2.write_string(row,12,bin_trans[data_in[key]["new_page?"]],text_format)
+            i+=1
+        worksheet2.write_string(row_old,13,key,text_format_sm)
+        row +=1
         
 def main():
     # Step 1: Read contents of existing ipr_read_data excel file and save to dictionary format
@@ -798,7 +825,6 @@ def main():
 
     # Step 4: Write all of our data to the output file
     write_ipr_data2(ipr_data, ipr_data.keys(), targets, out_file2)
-
 
 if __name__ == "__main__":
     main()
